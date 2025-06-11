7aec74c8e8d588279ac99e546bf4d7e0472353f1
GabMutter <gabriel.souza@sou.inteli.edu.br>
2025-06-11 10:28:03 -0300

    feat: conectando o front com o back

--- routes/frontRoute.js
+++ routes/frontRoute.js
@@ -5,18 +5,23 @@ const path = require('path');
 // Roteamento para páginas dinâmicas
 router.get('/', (req, res) => {
   res.render(path.join(__dirname, '../views/layout/main'), {
-    pageTitle: 'Página Inicial',
+    pageTitle: 'Página Login',
     content: path.join(__dirname, '../views/pages/page1')
+  });
+});
+
+router.get('/home', (req, res) => {
+  res.render(path.join(__dirname, '../views/layout/main'), {
+    pageTitle: 'Página Inicial',
+    content: path.join(__dirname, '../views/pages/page3')
   });
 });
 
 router.get('/cadastro', (req, res) => {
   res.render(path.join(__dirname, '../views/layout/main'), {
-    pageTitle: 'Página Inicial',
+    pageTitle: 'Página cadastro',
     content: path.join(__dirname, '../views/pages/page2')
   });
 });
-
-// Adicione outras rotas conforme necessário
 
 module.exports = router;
